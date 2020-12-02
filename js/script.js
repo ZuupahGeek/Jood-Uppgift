00f7008010be01dc247e6f6ae6021dcb04d0163f
Joachim Karlsson <joachim.karlsson@utb.ecutbildning.se>
2020-12-02 19:23:41 +0100

    slider

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
 AOS.init();
+
+
+$(function () {
+    $('.slick-slider').slick({
+        infinite:true,
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        arrows: false,
+        autoplay: false,
+        autoplaySpeed: 2000,
+        dots:true,
+        centerMode: true,
+        centerPadding: '0',
+
+    });
+});
+
+
+
